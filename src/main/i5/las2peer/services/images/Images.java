fb542c896a810365f94279ecb0bd6fa2416e13ae
CAEUser <CAEAddress@gmail.com>
2019-07-19 00:18:26 +0200

    Http Response[images] edited by Philipp Hossner Hossner

--- src/main/i5/las2peer/services/images/Images.java
+++ src/main/i5/las2peer/services/images/Images.java
@@ -193,7 +193,15 @@ public class Images extends RESTService {
     if(images_condition) {
       JSONObject imagesResult = new JSONObject();
 
-      
+      List<String> imageList = new ArrayList<>();
+      try {
+        while (results.next()) {
+          imageList.add(results.getString(1));
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+      imagesResult.put("images", imageList);      
 
       return Response.status(HttpURLConnection.HTTP_OK).entity(imagesResult.toJSONString()).build();
     }
