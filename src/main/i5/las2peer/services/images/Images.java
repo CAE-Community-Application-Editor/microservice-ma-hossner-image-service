e3637f9fd265c2870af01e69a7c84cbc459e6872
CAEUser <CAEAddress@gmail.com>
2019-07-18 20:13:05 +0200

    Code regeneration/Model synchronization

--- src/main/i5/las2peer/services/images/Images.java
+++ src/main/i5/las2peer/services/images/Images.java
@@ -225,7 +225,26 @@ public class Images extends RESTService {
 
   public Map<String, String> getCustomMessageDescriptions() {
     Map<String, String> descriptions = new HashMap<>();
-    
+        descriptions.put("SERVICE_CUSTOM_MESSAGE_1", "# HTTP Response Duration of Method getImages (GET)\n"
+        + "\n"
+        + "The number of milliseconds until the response is returned is logged according to the following JSON pattern:\n"
+        + "```json\n"
+        + "{ \"time\": <time_in_ms>, \"method\": <method_name>, \"resource\": <resource_name> }\n"
+        + "```\n"
+        + "## Example Measures\n"
+        + "### Response Duration\n"
+        + "Show in a line chart how long each request took to be processed.\n"
+        + "```sql\n"
+        + "SELECT TIME_STAMP, CAST(JSON_EXTRACT(REMARKS,\"$.time\") AS UNSIGNED) FROM MESSAGE WHERE EVENT=\"SERVICE_CUSTOM_MESSAGE_1\" AND SOURCE_AGENT = '$SERVICE$'\n"
+        + "```\n"
+        + "Visualization: line chart\n"
+        + "\n"
+        + "## Number of times getImages (GET) took longer than 400ms\n"
+        + "```sql\n"
+        + "SELECT COUNT(*) FROM MESSAGE WHERE EVENT=\"SERVICE_CUSTOM_MESSAGE_1\" AND SOURCE_AGENT = '$SERVICE$' AND CAST(JSON_EXTRACT(REMARKS,\"$.time\") AS UNSIGNED) > 400\n"
+        + "```\n"
+        + "Visualization: line chart\n");
+
     return descriptions;
   }
 
